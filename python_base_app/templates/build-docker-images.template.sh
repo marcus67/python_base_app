70b73a222671834095b166dd1a81954e728e5b96
Marcus Rickert <marcus.rickert@web.de>
2019-12-28 00:09:37 +0100

    * Add docker build to Circle CI configuration * Restrict uploading of docker images (only if password given)

--- python_base_app/templates/build-docker-images.template.sh
+++ python_base_app/templates/build-docker-images.template.sh
@@ -6,42 +6,45 @@ DEBIAN_PACKAGE_BASE_NAME={{ var.setup.debian_package_name }}_{{ var.setup.versio
 DEBIAN_PACKAGE_NAME=${BASE_DIR}/{{ var.setup.debian_build_dir}}/${DEBIAN_PACKAGE_BASE_NAME}.deb
 
 {% for (context, upload) in var.setup.docker_contexts %}
-echo "Build docker image in context directory '{{ context[0] }}'..."
+echo "Build docker image in context directory '{{ context }}'..."
 CONTEXT_DIR=${BASE_DIR}/{{ var.setup.docker_context_dir }}/{{ context }}
 ASSETS_DIR=${CONTEXT_DIR}/assets
+REVISION={{ var.setup.GIT_BRANCH }}-{{ var.setup.debian_package_revision }}
 
 if [ -d ${CONTEXT_DIR} ]; then
     cp -a ${DEBIAN_PACKAGE_NAME} ${ASSETS_DIR}/{{ var.setup.debian_package_name }}.deb
 fi
 
-docker build -t {{ var.setup.docker_registry_user }}/{{ context }}:{{ var.setup.debian_package_revision }} \
-    --build-arg TAG={{ var.setup.debian_package_revision }} ${CONTEXT_DIR}
-docker tag {{ var.setup.docker_registry_user }}/{{ context }}:{{ var.setup.debian_package_revision }} \
+docker build -t {{ var.setup.docker_registry_user }}/{{ context }}:${REVISION} \
+    --build-arg TAG=${REVISION} ${CONTEXT_DIR}
+{% if var.setup.GIT_BRANCH == var.setup.publish_latest_docker_image %}
+docker tag {{ var.setup.docker_registry_user }}/{{ context }}:${REVISION} \
     {{ var.setup.docker_registry_user }}/{{ context }}:latest
+{% endif -%}
 {% if upload -%}
-docker tag {{ var.setup.docker_registry_user }}/{{ context }}:{{ var.setup.debian_package_revision }} \
-    {{ var.setup.docker_registry}}/{{ var.setup.docker_registry_user }}/{{ context }}:{{ var.setup.debian_package_revision }}
-docker tag {{ var.setup.docker_registry_user }}/{{ context }}:{{ var.setup.debian_package_revision }} \
+docker tag {{ var.setup.docker_registry_user }}/{{ context }}:${REVISION} \
+    {{ var.setup.docker_registry}}/{{ var.setup.docker_registry_user }}/{{ context }}:${REVISION}
+{% if var.setup.GIT_BRANCH == var.setup.publish_latest_docker_image %}
+docker tag {{ var.setup.docker_registry_user }}/{{ context }}:${REVISION} \
     {{ var.setup.docker_registry}}/{{ var.setup.docker_registry_user }}/{{ context }}:latest
+{% endif -%}
 {% endif -%}
 {% endfor -%}
 
 if [ "${DOCKER_REGISTRY_PASSWORD}" == "" ] ; then
-    echo "No docker registry password set in DOCKER_REGISTRY_PASSWORD."
+    echo "No docker registry password set in DOCKER_REGISTRY_PASSWORD -> no upload of images"
 else
     echo "Logging into {{ var.setup.docker_registry}} as {{ var.setup.docker_registry_user }}..."
     docker login {{ var.setup.docker_registry}} --username  {{ var.setup.docker_registry_user }} --password ${DOCKER_REGISTRY_PASSWORD}
-fi
-
-{% for (context, upload) in var.setup.docker_contexts -%}
-{% if upload -%}
-echo "Uploading docker image in context directory '{{ context }}'..."
-docker push {{ var.setup.docker_registry}}/{{ var.setup.docker_registry_user }}/{{ context }}:{{ var.setup.debian_package_revision }}
-docker push {{ var.setup.docker_registry}}/{{ var.setup.docker_registry_user }}/{{ context }}:latest
-{% endif -%}
-{% endfor -%}
-
-if [ "${DOCKER_REGISTRY_PASSWORD}" != "" ] ; then
+    {% for (context, upload) in var.setup.docker_contexts -%}
+    {% if upload -%}
+    echo "Uploading docker image in context directory '{{ context }}'..."
+    docker push {{ var.setup.docker_registry}}/{{ var.setup.docker_registry_user }}/{{ context }}:${REVISION}
+    {% if var.setup.GIT_BRANCH == var.setup.publish_latest_docker_image %}
+    docker push {{ var.setup.docker_registry}}/{{ var.setup.docker_registry_user }}/{{ context }}:latest
+    {% endif -%}
+    {% endif -%}
+    {% endfor -%}
     echo "Logging out from {{ var.setup.docker_registry}}..."
     docker logout {{ var.setup.docker_registry}}
 fi
